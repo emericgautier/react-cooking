1139968146b9e887d65a6e03d724137436aaec64
emericgautier <emericgtr@gmail.com>
2022-10-09 12:27:31 +0200

    Update Meals.js

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -10,66 +10,7 @@ const Meals = () => {
           src="	https://www.themealdb.com/images/media/meals/58oia61564916529.jpg"
           alt="Photo title"
         />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fugit
-          nobis labore consequatur tempore rerum laborum magni, minima
-          reprehenderit repellat, iure, sint dolor unde hic tempora voluptatum
-          minus quas quis corporis obcaecati laboriosam porro dignissimos?
-          Aliquam voluptas sed vitae consequatur reprehenderit cum eaque
-          mollitia accusamus officiis sunt facilis perferendis aspernatur magnam
-          minima dolore ea, distinctio, exercitationem laudantium nisi
-          doloribus? Quae, quisquam. Explicabo, sint nostrum consequatur nam
-          nulla quas provident repellendus laboriosam error vitae porro tempora
-          itaque iusto neque, cum mollitia aliquid numquam voluptatum obcaecati
-          nesciunt modi eos ipsam debitis! Iste ratione soluta enim quam
-          repudiandae sed deleniti vitae corporis cum.
-        </p>
-      </div>
-
-      <div className="meal">
-        <h2>Title</h2>
-        <p>Subtitle</p>
-        <img
-          src="	https://www.themealdb.com/images/media/meals/58oia61564916529.jpg"
-          alt="Photo title"
-        />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fugit
-          nobis labore consequatur tempore rerum laborum magni, minima
-          reprehenderit repellat, iure, sint dolor unde hic tempora voluptatum
-          minus quas quis corporis obcaecati laboriosam porro dignissimos?
-          Aliquam voluptas sed vitae consequatur reprehenderit cum eaque
-          mollitia accusamus officiis sunt facilis perferendis aspernatur magnam
-          minima dolore ea, distinctio, exercitationem laudantium nisi
-          doloribus? Quae, quisquam. Explicabo, sint nostrum consequatur nam
-          nulla quas provident repellendus laboriosam error vitae porro tempora
-          itaque iusto neque, cum mollitia aliquid numquam voluptatum obcaecati
-          nesciunt modi eos ipsam debitis! Iste ratione soluta enim quam
-          repudiandae sed deleniti vitae corporis cum.
-        </p>
-      </div>
-
-      <div className="meal">
-        <h2>Title</h2>
-        <p>Subtitle</p>
-        <img
-          src="	https://www.themealdb.com/images/media/meals/58oia61564916529.jpg"
-          alt="Photo title"
-        />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fugit
-          nobis labore consequatur tempore rerum laborum magni, minima
-          reprehenderit repellat, iure, sint dolor unde hic tempora voluptatum
-          minus quas quis corporis obcaecati laboriosam porro dignissimos?
-          Aliquam voluptas sed vitae consequatur reprehenderit cum eaque
-          mollitia accusamus officiis sunt facilis perferendis aspernatur magnam
-          minima dolore ea, distinctio, exercitationem laudantium nisi
-          doloribus? Quae, quisquam. Explicabo, sint nostrum consequatur nam
-          nulla quas provident repellendus laboriosam error vitae porro tempora
-          itaque iusto neque, cum mollitia aliquid numquam voluptatum obcaecati
-          nesciunt modi eos ipsam debitis! Iste ratione soluta enim quam
-          repudiandae sed deleniti vitae corporis cum.
-        </p>
+        <p>Receipt</p>
       </div>
     </div>
   );
