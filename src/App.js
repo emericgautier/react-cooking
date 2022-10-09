9acf50f65b753b1881b3e3530f9bb08e7e816c17
emericgautier <emericgtr@gmail.com>
2022-10-09 16:20:21 +0200

    display data on screen and add an event on input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
-import Meals from "./components/Meals";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Meals from "./components/Meal";
 
 const App = () => {
+  const [mealsData, setMealsData] = useState([]);
+  const [inputSearch, setInputSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://www.themealdb.com/api/json/v1/1/search.php?s=" + inputSearch
+      )
+      .then((res) => setMealsData(res.data.meals));
+  }, [inputSearch]);
   return (
     <div className="container">
       <h1>React Cooking</h1>
-      <input type="text" placeholder="Taper le nom d'un aliment" />
-      <Meals />
+      <input
+        type="text"
+        placeholder="Taper le nom d'un aliment"
+        onChange={(e) => setInputSearch(e.target.value)}
+      />
+      <div className="meals">
+        {mealsData &&
+          mealsData
+            .slice(0, 24)
+            .map((meal) => <Meals key={meal.idMeal} meal={meal} />)}
+      </div>
     </div>
   );
 };
--- src/components/Meal.js
+++ src/components/Meal.js
@@ -3,13 +3,10 @@ import React from "react";
 const Meals = ({ meal }) => {
   return (
     <div className="meal">
-      <h2>Title</h2>
-      <p>Subtitle</p>
-      <img
-        src="	https://www.themealdb.com/images/media/meals/58oia61564916529.jpg"
-        alt="Photo title"
-      />
-      <p>Receipt</p>
+      <h2>{meal.strMeal}</h2>
+      <p>Origin : {meal.strArea}</p>
+      <img src={meal.strMealThumb} alt={"Photo " + meal.strMeal} />
+      <p>{meal.strInstructions}</p>
     </div>
   );
 };
